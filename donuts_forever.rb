8d0f621508e43f59ca7257c45944f7204a895fef
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-06-24 14:39:52 +0000

    Final Calculator code

---
+++ donuts_forever.rb
@@ -0,0 +1,29 @@
+# donut_dibs = {taylor: "Choc Glazed", melany: "Choc Sprinkles"}
+#
+# puts donut_dibs[:taylor]
+#
+# #donut_dibs.merge!({:jamie => "choc"})
+# donut_dibs[:jamie] = "Chocolate Glazed"
+# donut_dibs[:jamie] = "Sprinkles"
+# puts donut_dibs
+#
+# donut_dibs.each do |name, donut|
+#   puts "#{name} has #{donut}"
+# end
+
+
+komputers = {:nakhia => "nervous", :theresa => "Talkative", :dawsyn => "dramatic"}
+
+komputers[:nakhia] = "N'awesome"
+komputers["caroline Port"] = "confident"
+
+komputers.each do |name, adjective|
+  puts "#{name.capitalize} is very #{adjective}"
+end
+
+
+# sweet_nothings = {}
+# print sweet_nothings
+
+# print komputers.values
+# puts komputers.empty?
